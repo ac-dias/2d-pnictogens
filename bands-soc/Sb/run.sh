8154bad36949023614d51ccb3a219dcf1678fcaf
ac-dias <alexandre-131@hotmail.com>
2024-02-21 11:42:42 -0300

    bands-soc

---
+++ bands-soc/Sb/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -N  Sb-soc
+#PBS -e  job.err
+#PBS -o  job.o
+#PBS -q  workq 
+#PBS -l  nodes=01:ppn=16
+
+
+module load vasp-6
+module load vasp-pp
+
+ulimit -s unlimited
+
+
+
+export OMP_NUM_THREADS=1
+export I_MPI_FABRICS=shm
+#export I_MPI_HYDRA_TOPOLIB=ipl
+
+cd $PBS_O_WORKDIR
+
+cp INCAR-scf INCAR
+cp KPOINTS-scf KPOINTS
+
+mpirun -hostfile $PBS_NODEFILE -np $NCPUS vasp_ncl > log-scf.out
+
+cp INCAR-bands INCAR
+cp KPOINTS-bands KPOINTS
+
+mpirun -hostfile $PBS_NODEFILE -np $NCPUS vasp_ncl > log-bands.out
+
+vbands_k.x 6 > bandas.dat
+
+
+
+
